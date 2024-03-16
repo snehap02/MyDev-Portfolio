66635ae5686ff59df7bcf7728c25b5a6118a487c
Sneha Purkayastha <iamsneha0296@gmail.com>
2024-03-16 17:35:37 +0530

    new changes

--- src/components/Certification.js
+++ src/components/Certification.js
@@ -3,6 +3,7 @@ import React from 'react'
 const Certification = () => {
   return (
     <div className='certification pt-20 ml-10 px-3 sm:px-4 lg:ml-52 bg-[#1e1e1e] min-h-[100vh] min-w-[100vw] overflow-x-hidden'>Certification</div>
+    //sdfasf
   )
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,14 @@ module.exports = {
       screens:{
         '400': '400px',
         '1404': '1404px',
+      },
+      colors:{
+        'yellow': '#EEE4B1',
+        'blue': '#387ADF',
+        'sky': '#86B6F6',
+        'pink': '#E9A8F2',
+        'orange': '#F6995C',
+        'green': '#87A922'
       }
     },
   },
