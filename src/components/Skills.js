81d6464bfc61ad10e1f6adf63ec063d89e970bd3
Sneha Purkayastha <iamsneha0296@gmail.com>
2024-03-18 07:05:06 +0530

    started skills section

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -34,57 +34,14 @@ const Skills = () => {
         <h2>.</h2>
         <h2>.</h2>
       </div>
-      <div className="skillsSec flex flex-col">
-        <div className="languages w-64 sm:w-96 text-white text-lg">
-          <h1 className="uppercase">Languages I Speak</h1>
-          <div className="lang flex gap-6 flex-wrap md:flex-nowrap">
-            <h1 className="px-5 py-1 rounded-md bg-red-500 cursor-pointer text-sm">
-              html
-            </h1>
-            <h1 className="px-6 py-1 rounded-md bg-blue-600 cursor-pointer text-sm">
-              css
-            </h1>
-            <h1 className="px-8 py-1 rounded-md bg-amber-500 cursor-pointer text-sm">
-              JS
-            </h1>
+      <div className="skillsSection flex flex-col">
+        <div className="languageSection flex flex-col">
+          <h1 className="text-commentGreen uppercase italic tracking-wider">
+            // Languages I Speak
+          </h1>
+          <div className="html flex">
+            <h2 className="text-white">HTML</h2>
           </div>
-        </div>
-        <div className="frameworks w-64 text-white text-lg">
-          <h1 className="uppercase">Frameworks I Use</h1>
-          <div className="framework flex flex-wrap md:flex-nowrap gap-6">
-            <h1 className="px-5 py-1 rounded-md bg-sky-500 cursor-pointer text-sm">
-              TailwindCSS
-            </h1>
-            <h1 className="px-6 py-1 rounded-md bg-blue-500 cursor-pointer text-sm">
-              React
-            </h1>
-            <h1 className="px-8 py-1 rounded-md bg-purple-500 cursor-pointer text-sm">
-              Bootstrap
-            </h1>
-          </div>
-        </div>
-        <div className="tools w-full text-white text-lg">
-          <h1 className="uppercase">Tools In My Toolkit</h1>
-          <div className="tool flex flex-wrap md:flex-nowrap gap-6">
-            <h1 className="px-5 py-1 rounded-md bg-sky-500 cursor-pointer h-8 text-sm">
-              Git
-            </h1>
-            <h1 className="px-6 py-1 rounded-md bg-blue-500 cursor-pointer h-8 text-sm">
-              Github
-            </h1>
-            <h1 className="px-10 py-1 rounded-md bg-purple-500 cursor-pointer h-8 text-sm">
-              VS Code
-            </h1>
-            <h1 className="px-8 py-1 rounded-md bg-purple-500 cursor-pointer h-8 text-sm">
-              Wordpress
-            </h1>
-            <h1 className="px-8 py-1 rounded-md bg-purple-500 cursor-pointer h-8 text-sm">
-              Codepen
-            </h1>
-          </div>
-        </div>
-        <div className="designs w-64 text-white text-lg">
-          <h1 className="uppercase">Design Tools I Navigate</h1>
         </div>
       </div>
     </div>
