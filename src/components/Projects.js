73a63b32d2f94b9231e2f119b3094a18e600f12b
Sneha Purkayastha <iamsneha0296@gmail.com>
2024-03-20 13:20:16 +0530

    started projects section

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -2,7 +2,44 @@ import React from 'react'
 
 const Projects = () => {
   return (
-    <div className='projects pt-20 ml-10 px-3 sm:px-4 lg:ml-52 bg-[#1e1e1e] min-h-[100vh] min-w-[100vw] overflow-x-hidden'>Projects</div>
+    <div className='projects pt-20 ml-10 px-3 sm:px-4 lg:ml-52 bg-[#1e1e1e] min-h-[100vh] min-w-[100vw] overflow-x-hidden flex'>
+      <div className="nums bg-[#1e1e1e] w-10 md:w-16 h-full flex flex-col items-center text-neutral-700 px-4 lg:-ml-4">
+        <h2>1</h2>
+        <h2>2</h2>
+        <h2>3</h2>
+        <h2>4</h2>
+        <h2>5</h2>
+        <h2>6</h2>
+        <h2>7</h2>
+        <h2>8</h2>
+        <h2>9</h2>
+        <h2 className="-ml-2">10</h2>
+        <h2 className="-ml-2">11</h2>
+        <h2 className="-ml-2">12</h2>
+        <h2 className="-ml-2">13</h2>
+        <h2 className="-ml-2">14</h2>
+        <h2 className="-ml-2">15</h2>
+        <h2 className="-ml-2">16</h2>
+        <h2 className="-ml-2">17</h2>
+        <h2 className="-ml-2">18</h2>
+        <h2 className="-ml-2">19</h2>
+        <h2 className="-ml-2">20</h2>
+        <h2 className="-ml-2">21</h2>
+        <h2 className="-ml-2">22</h2>
+        <h2 className="-ml-2">23</h2>
+        <h2 className="-ml-2">24</h2>
+        <h2>.</h2>
+        <h2>.</h2>
+        <h2>.</h2>
+        <h2>.</h2>
+        <h2>.</h2>
+      </div>
+      <div className="projectsSec flex flex-col w-[80%]">
+      <div className="heading uppercase text-xl 500:text-3xl lg:text-4xl mt-2 md:tracking-wider text-commentGreen -ml-4 font-semibold italic text-center">
+          <h1>// Projects</h1>
+        </div>
+      </div>
+    </div>
   )
 }
 
